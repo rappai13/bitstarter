f77c76e001951a86cb025a77537c59e7669f565e
Ubuntu <ubuntu@ip-172-31-14-16.us-west-2.compute.internal>
2013-07-23 21:23:38 +0000

    Fixed 'epress is not defined'...

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs')
-var app = express.createServer(epress.logger());
+var app = express.createServer(express.logger());
 
 
 app.get('/', function(request, response) {
