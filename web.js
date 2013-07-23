b588ea3859baaf5120f874d414dc1460ad2969c7
Ubuntu <ubuntu@ip-172-31-14-16.us-west-2.compute.internal>
2013-07-23 20:28:34 +0000

    Won't work

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var index = fs.readFileSync('index.html' , 'utf-8');
+  response.send(index);
 });
 
 var port = process.env.PORT || 5000;
